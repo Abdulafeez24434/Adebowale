cb47fbdcd1bda8e2c3c2295e9fec9557c32a98cf
Abdulafeez24434 <abdulafeezadeyemi27@gmail.com>
2019-11-25 10:26:24 +0100

    assignment

---
+++ Assignments.py
@@ -0,0 +1,26 @@
+# # name = input ("What is your name?")
+# # age =int(input ("how old are are you?"))
+# # current_year = 2019
+# # if(current_year%4==0 and current_year%100!=0 or current_year%400==0):
+# #     comment = f"hello{name} you are {age}, you were born in {2019-int(age)}. Your birth year is a leap year"
+# # else:
+# #     comment =  f"hello{name} you are {age}, you were born in {2019-int(age)}. Your birth year isn't leap year"
+# # print (comment)
+
+# word = input('please enter word : ')
+# reversed_word = word[::-1]
+# print(f"{word} is a palindrome : {word ==reversed_word}")
+
+# name = ["Shade", "Muyiwa", "Tayo", "Kemi"]
+# m = ["Mr"]
+# f = ["Mrs"]
+# mapped = map(lambda x :  +x, name)
+# print(list(mapped))
+
+# three_friends = ["Shola", "Ayo", "Tunde"]
+# friends_no = (len(three_friends))
+# candies = int(input("number of candies : "))
+# g = int(candies/friends_no)
+# remainder = int(candies % 3)
+# statement = f'number of candy to be shared is : {g}\nnumber of candies to be crushed : {remainder}'
+# print(statement)
