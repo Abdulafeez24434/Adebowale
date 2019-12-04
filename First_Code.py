c54d7911dbe539ddc4f571421aef9ab558e9d23d
Abdulafeez24434 <abdulafeezadeyemi27@gmail.com>
2019-12-04 11:33:17 +0100

    First

--- First_Code.py
+++ First_Code.py
@@ -292,40 +292,221 @@
 # s = ('foo' if (x == 1) else ('bar' if (x == 2) else ('baz' if (x == 3) else ('qux'if (x == 4) else 'quux'))))
 # print(s)
 
-# question1 = input('Are you okay ? :')
+# question1 = input('Are you okay ? : ')
 # if question1 == 'True' :
 #     print('please get a life')
 # else: 
-#     print('do you have pain')
-#     question2 = input('do you have pain ? :')
+#     question2 = input('do you have pain ? : ')
 #     if question2 == 'False' :
 #         print('unable to diagose now')
 #     else:
-#         print('did you sleep well')
-#         question3 = input('did you sleep well " :')
+#         question3 = input('did you sleep well " : ')
 #         if question3 == 'False' :
 #             print('try to sleep')
 #         else:
-#             print('have you done hardwork')
-#             question4 = input('have you done hardwork ? :')
+#             question4 = input('have you done hardwork ? : ')
 #             if question4 == 'True' :
 #                 print('try to sleep')
 #             else:
-#                 print('do you have a fever')
-#                 question5 = input('do you have a fever ? :')
+#                 question5 = input('do you have a fever ? : ')
 #                 if question5 == 'False' :
 #                     print('inconclusive please see a doctor')
 #                 else:
-#                     print('are you vomitting')
-#                     question6 = input('are you vomitting ? :')
+#                     question6 = input('are you vomitting ? : ')
 #                     if question6 == 'False' : 
 #                         print('take some anti-malaria')
 #                     else:
 #                         print('please see a doctor')
 
-            
-        
-    
+# a = ['foo', 'bar', 'baz']
+# for i in a :
+#     print(i)
+
+# mean = sum(range(20)) / len(range(20))
+# for i in range(20):
+#     print(i, i-mean)
+
+# print("x".center(4), "|","x_bar".center(7), "|", "(x_bar)**2".center(11),  sep = "")
+# print("___".center(4), "|","____".center(7), "|","_______".center(11), sep = "")
+# n = 5
+# mean = sum(range(n)) / len(range(n))
+# for i in range(n):
+#     print(f"{i}".center(4), "|",f"{i-mean}".center(7), "|",f"{(i-mean)**2}".center(11), sep = "")
+
+# names = ["John","Clam"]
+# for i in names:
+#     print("Hello,",i)
+
+# words = input('type a words : ')
+# vowel_sounds = ['a','e','i','o','u']
+# total_vowels = 0
+# for i in words:
+#     if i in vowel_sounds:
+#         print(i)
+#         total_vowels = total_vowels + 1
+# print(f"Total vowels : {total_vowels}")
+
+# words = input('type a words : ')
+# vowel_sounds = ['a','e','i','o','u']
+# total_vowels = 0
+# for vowel_sounds in vowel_sounds:
+#     if vowel_sounds in words:
+#         print(vowel_sounds)
+#         count = words.count(vowel_sounds)
+#         total_vowels = total_vowels + count
+# print(f"Total vowels : {total_vowels}")
+
+# words = input('type a words : ')
+# vowel_sounds = ['a','e','i','o','u']
+# total_vowels = 0
+# print(f"vowel | Count")
+# print(f"______|______")
+# for vowel_sounds in vowel_sounds:
+#     if vowel_sounds in words:
+#         count = words.count(vowel_sounds)
+#         total_vowels = total_vowels + count
+#         print(f"{vowel_sounds.center(6)}|{str (count).center(6)}")
+# print(f"Total vowels : {total_vowels}")
+
+# for i in range(10):
+#     print(i)
+#     if i ==5:
+#          break
+
+# word = input("Enter word : ")
+# for i in word:
+#     print(i)
+#     if i =='?':
+#         break
+
+# filename = "twist.txt"
+# raw_data = open(filename, "r")
+# data = raw_data.readlines()
+# for line in data:
+#     print(line)
+#     print()
+#     if "canoed" in line:
+#         break
+
+# scores = [20, 30, 70, 10, 67, 50]
+# import random
+# for score in scores:
+
+#     c_assess = random.randint(10,30)
+#     f_score = score + c_assess
+
+#     marked_up = int(f_score * 1.2)
+#     if f_score > 70:
+#         print(score, c_assess, f_score, f_score)
+#         continue
+
+#     print(scores, c_assess, f_score, marked_up, "marked_up")
+
+# loan_amount = 300000
+# rate = 0.05
+# time = 2
+# payments_per_year = 12
+# principal = loan_amount
+
+# repayment = (principal*(rate/payments_per_year))/(1-(1+(rate/payments_per_year))**((payments_per_year*-1)*time))
+
+# no_payments = time * payments_per_year
+
+# print("Period".center(5),"AMOUNT".center(11),"INTEREST".center(11),"PRINCIPAL".center(11), "BALANCE".center(11),"\n ")
+
+# loan_interest = 0
+# for payment in range(no_payments + 1):
 
 
-   
+#     if payment == 0:
+#         print(str(payment).center(5),"-".center(11), "-".center(11), "-".center(11), str(loan_amount).center(11))
+#     else:
+#         interest = loan_amount * (rate/payments_per_year)
+#         loan_amount = loan_amount - (repayment - interest)
+#         principal = (repayment - interest)
+#         principal = round(principal,2)
+#         loan_interest += interest
+#         repayment, interest, loan_amount = round(repayment,2), round(interest,2), round(loan_amount,2)
+
+#         print(str(payment).center(5),str(repayment).center(11), str(interest).center(11), str(principal).center(11), str(loan_amount).center(11))
+# print(loan_interest)
+
+# import random
+# choices = ["G", "G", "C"]
+# wins = 0
+# loses = 0
+# for i in range(100):
+#     choice = random.choice(choices)
+#     if choice == "":
+#         print("Yesss!!! You won..!!!")
+#         wins+=1
+#     else :
+#         print("ooops You lost..!!!")
+#         loses+=1
+# print("wins: ", wins, "Loses: ", loses)
+
+# import random
+
+# ## HOLDER ###
+# choices = ["G", "G", "C"]
+# random.shuffle(choices)
+
+
+# trials = 0
+# wins = 0
+# while trials < 1000:
+#   trials += 1
+#   guest_choice = random.choice(choices)
+
+#   if guest_choice == "C":
+#     # print("Win!!")
+#     wins += 1
+#   elif guest_choice == "G":
+#     # print("Loose!!")
+#     pass
+
+# prob_success = wins/trials
+# print(f"Holder : {prob_success * 100}%")
+
+
+# trials = 0
+# wins = 0
+
+# while trials < 1000:
+#   trials += 1
+#   guest_choice = random.choice(choices)
+
+#   temp_choices = choices.copy()
+#   temp_choices.remove(guest_choice)
+#   temp_choices.remove("G")
+
+#   guest_choice = temp_choices[0]
+
+#   if guest_choice == "C":
+#     # print("Win!!")
+#     wins += 1
+#   elif guest_choice == "G":
+#     # print("Loose!!")
+#     pass
+
+# prob_success = wins/trials
+# print(f"Switcher : {prob_success * 100}%")
+
+# n = 5
+# while n > 0:
+#     n -=1
+#     print(n)
+
+# n = 5
+# for i in range(n):
+#     n = n-1
+#     print(n)
+
+# x = "boy toys junk"
+# y = "mum girl luch"
+# n = 0
+# word_length = len(x)
+# while n < word_length:
+#     print(x[n], y[n])
+#     n+=1
+
